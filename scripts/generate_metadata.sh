524e8b35a441b6b3cca8ec7c61f80a5ecb9033b6
Eugene Kravchenko <Zzzheka97@gmail.com>
2021-08-14 18:59:44 +0300

    Delete generate_metadata.sh

--- scripts/generate_metadata.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-VERSION="$1"   && shift
-UUID="$1"      && shift
-
-TEMPLATE_DIR="$( dirname "$0" )/../templates/extension"
-
-sed "s/@VERSION@/${VERSION}/g" < "${TEMPLATE_DIR}/metadata.json" |\
-sed "s/@UUID@/${UUID}/g"                                         
